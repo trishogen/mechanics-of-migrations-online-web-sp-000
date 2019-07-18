a58aafd99106ac620f1e857adeae4fba3a6c6625
Trish Ogen <patriciaogen@gmail.com>
2019-07-18 23:17:10 +0000

    Automatically backed up by Learn

--- db/01_create_artists.rb
+++ db/01_create_artists.rb
@@ -1,6 +1,6 @@
 # db/migrate/01_create_artists.rb
 
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
       t.string :name
