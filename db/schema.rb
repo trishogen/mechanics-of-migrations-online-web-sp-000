af585f80d7b15547e87ddc9ac8d45a644f4f25ff
Trish Ogen <patriciaogen@gmail.com>
2019-07-18 23:18:06 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "age"
+    t.string "hometown"
+  end
 
 end
